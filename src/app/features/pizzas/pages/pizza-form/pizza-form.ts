8e9eb1efa74ef38d462b5205ae781d53a2915512
isagrt <isadoragoedert05@gmail.com>
2026-04-07 10:38:24 -0300

    criando arquivo frontend angular

---
+++ src/app/features/pizzas/pages/pizza-form/pizza-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-form',
+  imports: [],
+  templateUrl: './pizza-form.html',
+  styleUrl: './pizza-form.css',
+})
+export class PizzaForm {}
