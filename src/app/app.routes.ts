5110ea0b3e8e135663c337e8a385b1b008db693f
isagrt <isadoragoedert05@gmail.com>
2026-04-07 10:47:02 -0300

    configurando rotas e autenticação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/pages/login/login';
+import { AccessDenied } from './features/auth/pages/access-denied/access-denied';
+import { MainLayout } from './layout/components/main-layout/main-layout';
+import { authGuard } from './core/guards/auth-guard';
+import { Home } from './features/dashboard/pages/home/home';
+import { PizzaList } from './features/pizzas/pages/pizza-list/pizza-list';
+import { PizzaForm } from './features/pizzas/pages/pizza-form/pizza-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+        {path: '', redirectTo: 'login', pathMatch:'full'},
+        {   
+            path:'login', 
+            component: Login
+        },
+        {   
+            path:'acesso-negado', 
+            component: AccessDenied
+        },
+        {   
+            path:'', 
+            component: MainLayout,
+            canActivate: [authGuard],
+            children: [
+                {
+                    path:'home',
+                    component:Home
+                },
+                {
+                    path:'pizza',
+                    component:PizzaList
+                },
+                {
+                    path:'pizza/novo',
+                    component:PizzaForm
+                },
+                {
+                    path:'pizza/editar/:id',
+                    component:PizzaForm
+                },
+            ]
+        },
+        {
+            path:'**',
+            redirectTo: 'login'
+        }
+];
--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -1,6 +1,14 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {}
+export class Auth {
+
+  private http = inject(HttpClient);
+  private router = inject(Router);
+  private apiUrl = 'http://localhost:5157/auth'
+
+}
